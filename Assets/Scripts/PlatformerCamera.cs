57b91a2b94a52076a35d1ca247d8218180cf6e23
Jaden Balogh <jadenbalogh@gmail.com>
2020-06-11 17:08:59 -0700

    Update controller and add camera follow

---
+++ Assets/Scripts/PlatformerCamera.cs
@@ -0,0 +1,25 @@
+﻿/* 
+    Mario-style camera-follow script by Jaden Balogh.
+*/
+
+using UnityEngine;
+
+public class PlatformerCamera : MonoBehaviour
+{
+    [Tooltip("The target object that the camera will be following (typically the player).")]
+    public Transform target;
+    [Tooltip("Does the camera follow the target to the left?")]
+    public bool allowBacktracking = false;
+    [Tooltip("The time it takes for the camera to move to the target's current location.")]
+    public float followTime = 0.1f;
+
+    private Vector3 currentVelocity;
+
+    void FixedUpdate() {
+        // Follow the target based on set conditions
+        if (allowBacktracking || target.position.x > transform.position.x) {
+            Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, followTime);
+        }
+    }
+}
--- Assets/Scripts/PlatformerController.cs
+++ Assets/Scripts/PlatformerController.cs
@@ -2,20 +2,22 @@
     Mario-style platformer player movement controller by Jaden Balogh.
 */
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformerController : MonoBehaviour
 {
-    [Tooltip("The maximum horizontal speed of the player.")]
-    public float maxMoveSpeed = 6f;
-    [Tooltip("The maximum downwards falling speed of the player.")]
-    public float maxFallSpeed = 8f;
+    [Tooltip("Can the player move past the left-hand side of the screen?")]
+    public bool allowBacktracking = false;
     [Tooltip("The strength of the player's horizontal acceleration using the arrow keys.")]
     public float moveAcceleration = 15f;
+    [Tooltip("The maximum horizontal speed of the player.")]
+    public float maxMoveSpeed = 6f;
+    [Tooltip("The horizontal drag slowing the player over time.")]
+    public float horizontalDrag = 1f;
     [Tooltip("The strength of the constant downwards acceleration (gravity) on the player. NOTE: This controller overrides normal physics gravity on the player.")]
     public float fallAcceleration = 15f;
+    [Tooltip("The maximum downwards falling speed of the player.")]
+    public float maxFallSpeed = 8f;
     [Tooltip("The maximum height the player can jump when the spacebar is held for the full duration.")]
     public float maxJumpHeight = 3.2f;
     [Tooltip("Multiplies the strength of the downwards counter-force applied to the player if they cancel a jump early by releasing the spacebar.")]
@@ -24,10 +26,12 @@ public class PlatformerController : MonoBehaviour
     private float jumpForce = 1f;
     private bool isJumpKeyHeld = false;
     private bool isGrounded = true;
+    private float xOffset = 0.5f;
     private Rigidbody2D rb2D;
 
     void Awake() {
         rb2D = GetComponent<Rigidbody2D>();
+        xOffset = GetComponent<BoxCollider2D>().size.x / 2;
     }
 
     void Start() {
@@ -50,8 +54,26 @@ public class PlatformerController : MonoBehaviour
     }
 
     void FixedUpdate() {
-        // Apply horizontal movement force
-        rb2D.AddForce(Input.GetAxisRaw("Horizontal") * moveAcceleration * Vector2.right);
+        // Determine if the player is at the left bound and backtracking is disabled
+        float leftBound = Camera.main.ScreenToWorldPoint(Vector2.zero).x + xOffset;
+        bool isBlocked = !allowBacktracking && transform.position.x <= leftBound;
+
+        // Cancel horizontal movement if blocked
+        if (isBlocked) {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            transform.position = new Vector3(leftBound, transform.position.y);
+        }
+
+        // Apply horizontal movement force if not blocked
+        float moveForce = Input.GetAxisRaw("Horizontal") * moveAcceleration;
+        moveForce = isBlocked ? Mathf.Max(0, moveForce) : moveForce;
+        rb2D.AddForce(moveForce * Vector2.right);
+
+        // Apply horizontal drag when no force is applied
+        float xDir = rb2D.velocity.normalized.x;
+        if (Mathf.Abs(xDir) > 0) {
+            rb2D.AddForce(horizontalDrag * -xDir * Vector2.right);
+        }
 
         // Apply jump cancel force if spacebar is released early
         if (rb2D.velocity.y > 0.1f && !isJumpKeyHeld) {
@@ -63,13 +85,13 @@ public class PlatformerController : MonoBehaviour
             rb2D.AddForce(-jumpForce * Vector2.up);
         }
 
-        // Apply falling force
+        // Apply falling force (gravity)
         rb2D.AddForce(-fallAcceleration * Vector2.up);
 
-        // Clamp velocity
-        Vector2 clampedX = Mathf.Clamp(rb2D.velocity.x, -maxMoveSpeed, maxMoveSpeed) * Vector2.right;
-        Vector2 clampedY = Mathf.Max(rb2D.velocity.y, -maxFallSpeed) * Vector2.up;
-        rb2D.velocity = clampedX + clampedY;
+        // Clamp velocity to set bounds
+        Vector2 velocityX = Mathf.Clamp(rb2D.velocity.x, -maxMoveSpeed, maxMoveSpeed) * Vector2.right;
+        Vector2 velocityY = Mathf.Max(rb2D.velocity.y, -maxFallSpeed) * Vector2.up;
+        rb2D.velocity = velocityX + velocityY;
     }
 
     void OnCollisionStay2D(Collision2D col) {
