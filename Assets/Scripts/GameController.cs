b54b6373f8a497f981828878e81f9cba54da4a7f
Jaden Balogh <jadenbalogh@gmail.com>
2020-06-12 20:45:29 -0700

    Add HUD

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,9 +8,13 @@ public class GameController : MonoBehaviour
     public static GameController instance;
 
     public string startSceneName = "Level1";
+    public int startTime = 120;
     public int totalLives = 3;
 
-    private int remainingLives;
+    private int level;
+    private int score;
+    private int time;
+    private int lives;
 
     void Awake() {
         // Set up the GameController as a singleton
@@ -24,23 +28,60 @@ public class GameController : MonoBehaviour
     }
 
     void Start() {
-        remainingLives = totalLives;
+        SetLives(totalLives);
+        SetTime(startTime);
+        StartCoroutine(LevelTimer());
     }
 
     public void RestartGame() {
         SceneManager.LoadScene(startSceneName);
+        SetLives(totalLives);
+        SetTime(startTime);
     }
 
     public void RestartLevel() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SetTime(startTime);
+    }
+
+    public void AddScore(int score) {
+        SetScore(this.score + score);
+    }
+
+    public void SetLevel(int level) {
+        this.level = level;
+        HUD.instance.SetLevel(this.level);
+    }
+
+    public void SetScore(int score) {
+        this.score = score;
+        HUD.instance.SetLives(this.score);
+    }
+
+    public void SetTime(int time) {
+        this.time = time;
+        HUD.instance.SetTime(this.time);
+    }
+
+    public void SetLives(int lives) {
+        this.lives = lives;
+        HUD.instance.SetLives(this.lives);
     }
 
     public void Die() {
-        remainingLives--;
-        if (remainingLives <= 0) {
+        SetLives(lives - 1);
+        if (lives <= 0) {
             RestartGame();
         } else {
             RestartLevel();
         }
+    }
+
+    private IEnumerator LevelTimer() {
+        while (time > 0) {
+            yield return new WaitForSeconds(1);
+            SetTime(time - 1);
+        }
+        Die();
     }
 }
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public static HUD instance;
+
+    public Text levelText;
+    public Text scoreText;
+    public Text timeText;
+    public Text livesText;
+
+    void Awake() {
+        // Set up the HUD as a singleton
+        if (instance != null) {
+            Destroy(gameObject);
+            return;
+        } else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SetLevel(int level) {
+        levelText.text = "LEVEL\n" + level;
+    }
+
+    public void SetScore(int score) {
+        scoreText.text = "SCORE\n" + score;
+    }
+
+    public void SetTime(int time) {
+        timeText.text = "TIME\n" + time;
+    }
+
+    public void SetLives(int lives) {
+        livesText.text = "LIVES\n" + lives;
+    }
+}
